761d8f6458af4a315369337843149a0e1e6b374a
npc <nhanvo.job@gmail.com>
2026-07-12 14:24:14 +0700

    first commit

---
+++ src/main/java/vn/npc/ads_schedule/AdsScheduleApplication.java
@@ -0,0 +1,13 @@
+package vn.npc.ads_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/npc/ads_schedule/AdsScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.npc.ads_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
